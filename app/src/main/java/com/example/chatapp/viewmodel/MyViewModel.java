b3eaff8acb0451501acd334207b861f4f6234ca3
DELL <omnianabil1920@gmail.com>
2024-07-23 12:15:11 +0300

    created view model

--- app/src/main/java/com/example/chatapp/viewmodel/MyViewModel.java
+++ app/src/main/java/com/example/chatapp/viewmodel/MyViewModel.java
@@ -1,4 +1,28 @@
 package com.example.chatapp.viewmodel;
 
-public class MyViewModel  {
+import android.app.Application;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.chatapp.repository.Repository;
+
+public class MyViewModel extends AndroidViewModel{
+    Repository repository;
+
+    public MyViewModel(@NonNull Application application) {
+        super(application);
+        repository = new Repository();
+    }
+    public void signUpAnonymousUser(){
+        Context c = this.getApplication();
+        repository.firebaseAnonymousAuth(c);
+    }
+    public String getCurrentUserId(){
+        return repository.getCurrentUserId();
+    }
+    public void signOut(){
+        repository.signOut();
+    }
 }
